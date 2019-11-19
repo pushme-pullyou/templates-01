46da3873546d5ff86e79624fc73c48706a2f9d7e
Theo Armour <theo-armour@users.noreply.github.com>
2019-11-18 18:22:43 -0800

    edde first commit

---
+++ mozilla-text-editor/v-0-04/src/fga-files-github-api.js
@@ -0,0 +1,207 @@
+// Copyright pushMe-pullYou authors. MIT license.
+// jshint esversion: 6
+// jshint loopfunc: true
+/* globals FGAdivFolders, FGAdivFiles, FGAdivBreadcrumbs */
+
+"use strict";
+
+const FGA = {};
+
+FGA.urlSourceCodeImage = "src/github-mark-32.png";
+FGA.iconInfo = `<img src="${FGA.urlSourceCodeImage}" alt="GitHub logo" height=18 style=opacity:0.5 >`;
+FGA.pathPrevious = "";
+
+FGA.getMenuFilesGithubApi = function() {
+	window.addEventListener("hashchange", FGA.onHashChange, false);
+
+	const htm = `
+		<div id = "FGAdivBreadcrumbs" ></div>
+
+		<details open class=details__secondary >
+
+			<summary class=summary__secondary >Folders</summary>
+
+			<div id = "FGAdivFolders" class=nav-menu-div ></div>
+
+		</details>
+
+		<details open  class=details__secondary >
+
+			<summary  class=summary__secondary >Files</summary>
+
+			<div id = "FGAdivFiles" class=nav-menu-div></div>
+
+		</details>
+
+	`;
+
+	return htm;
+};
+
+FGA.onHashChange = function() {
+	const url = !location.hash ? "" : location.hash.slice(1);
+
+	FGA.path = url.lastIndexOf("/") > 0 ? url.slice(0, url.lastIndexOf("/")) : "";
+	//FGA.path = url.lastIndexOf("/") > 0 ? url.slice(0, url.lastIndexOf("/")) : COR.pathRepo;
+
+	// if (FGA.path === FGA.pathPrevious && FGAdivFiles.innerHTML !== "") {
+	// 	return;
+	// }
+
+	FGA.fetchTree(FGA.path);
+
+	//FGA.pathPrevious = FGA.path;
+};
+
+FGA.fetchTree = function(path = "") {
+	FGA.setBreadcrumbs(path);
+
+	const url = `https://api.github.com/repos/${COR.user}/${COR.repo}/contents/data/days-of-year-md/${path}`;
+	//console.log('url', url);
+
+	fetch(new Request(url))
+		.then(response => response.json())
+		.then(json => FGA.callbackGitHubPathFileNames(json));
+};
+
+FGA.setBreadcrumbs = function(path) {
+	let folders = path ? path.split("/") : [];
+
+	if (folders[1] === "days-of-year-md") {
+		folders = folders.slice(2);
+	}
+	//console.log( "folders", folders );
+
+	const htmFolders = folders
+		.map((folder, i) => {
+			//console.log('folder', folder);
+			const str = folders.slice(0, i + 1).join("/");
+			//console.log( 'str', str );
+
+			return `<a href="#${str}/"); >${decodeURI(folder)}</a> &raquo; `;
+		})
+		.join("");
+
+	FGAdivBreadcrumbs.innerHTML = `
+		<div style="margin:0.2rem 1rem;" >
+			<b>
+				<a href=# title="Click to return to home folder" >
+					<span style=font-size:28px >&#x2302</span>
+				</a> &raquo;
+				${htmFolders}
+			</b>
+		</div>
+	`;
+};
+
+FGA.callbackGitHubPathFileNames = function(items) {
+	//console.log( "items", items );
+
+	if (items.message) {
+		console.log("error", items.message);
+		return;
+	}
+
+	items = Array.isArray(items) ? items : [items];
+
+	FGAdivFolders.innerHTML = FGA.getFolders(items);
+
+	FGAdivFiles.innerHTML = FGA.getFiles(items);
+
+	let name = location.hash
+		? location.hash
+				.slice(1)
+				.split("/")
+				.pop()
+		: "README.md";
+
+	name = name ? name : "README.md";
+	//console.log( "name", name );
+
+	const divs = FGAdivFiles.querySelectorAll("div.FGAitem");
+
+	divs.forEach(
+		dv =>
+			(dv.parentElement.style.backgroundColor =
+				dv.innerText.trim() === name ? "lightgreen" : "")
+	);
+};
+
+FGA.getFolders = function(items) {
+	const htm = items
+		.filter(item => item.type === "dir" && !COR.ignoreFolders.includes(item.name))
+		.map(
+			item => `
+			<div style="margin:0.2rem 1rem;" title="Click to open the folder">
+				<a href="#${item.path.slice(21)}/"; >
+					&#x1f4c1; ${item.name}
+				</a>
+			</div>
+		`
+		)
+		.join("");
+
+	return htm;
+};
+
+FGA.getFiles = function(items = []) {
+	const htm = items
+		.filter(item => item.type === "file" && !COR.ignoreFiles.includes(item.name))
+		.map(item => {
+
+			FGA.urlGitHubSource = `https://github.com/${COR.user}/${COR.repo}/
+				/blob/${FGA.branch}/${item.path}`;
+
+			return `
+				<div style="margin:0.5rem 1rem"; >
+					<div style=display:inline-block; >
+						<a href="${FGA.urlGitHubSource}"
+							target=_top title="View or edit source code" >
+							${FGA.iconInfo}
+						</a>
+					</div>
+					<div style=display:inline-block; class=FGAitem >
+						<a href=#${item.path.slice(21)}
+						title="Click to view file" >${item.name}
+						</a>
+					</div>
+				</div>
+			`;
+		})
+		.join("");
+
+	return htm;
+};
+
+
+
+FGA.setGitHubAccessToken = function () {
+
+	console.log( 'accessToken', FGAinpGitHubApiKey.value );
+
+	localStorage.setItem( "githubAccessToken", FGAinpGitHubApiKey.value );
+
+	//FGA.accessToken = accessToken;
+
+};
+
+
+
+FGA.test = function() {
+	const links = FGAdivFiles.querySelectorAll("a");
+	console.log("links", links);
+
+	let i = 1;
+
+	nextLink();
+
+	function nextLink() {
+		links[i].click();
+
+		if (i < links.length - 1) {
+			i += 2;
+
+			setTimeout(nextLink, 1000);
+		}
+	}
+};
---
+++ mozilla-text-editor/v-0-05/src/page-editor.js
@@ -0,0 +1,193 @@
+// https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Editable_content
+// https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Midas
+
+const pathApi = "https://api.github.com/repos/theo-armour/theo-armour.github.io/contents/data/days-of-year-md/";
+
+let logFileContent;
+let logFileSha;
+let decodedData;
+
+var oDoc, sDefTxt;
+
+function initDoc() {
+
+	oDoc = document.getElementById("textBox");
+	sDefTxt = oDoc.innerHTML;
+	if (switchBox.checked) {
+		setDocMode(true);
+	}
+
+	window.addEventListener("hashchange", onHashChange, false)
+}
+
+function onHashChange() {
+
+	url = location.hash.slice(1);
+
+	if (url.toLowerCase().endsWith(".md") === false) { return; }
+
+	getHtmlFile(url);
+
+}
+
+
+function getHtmlFile(url) {
+
+	fetch(new Request( pathApi + url))
+		.then(response => response.json())
+		.then(data => {
+
+			//console.log( data );
+
+			//divResponseGet.innerText = (JSON.stringify( data, null, "\t"  ) );
+
+			//console.log( 'dc', data.content );
+
+			decodedData = window.atob(data.content);
+
+			textBox.innerHTML = decodedData;
+
+			logFileSha = data.sha;
+
+		}).catch((err) => {
+
+			console.warn('Something went wrong.', err);
+		});
+
+}
+
+
+
+function putHtmlFile() {
+
+	url = location.hash.slice(1);
+	//const url = "https://api.github.com/repos/theo-armour/theo-armour.github.io/contents/first-post.jsonl";
+
+	const request = new Request(pathApi + url );
+
+	content = textBox.innerHTML
+
+	//divToSend.innerHTML = content;
+
+	codedData = window.btoa(content); // encode the string
+
+	fetch(request, {
+		method: "PUT",
+		headers: { "Authorization": "token " + FGA.accessToken, "Content-Type": "text/plain" },
+		body: JSON.stringify({
+			"branch": "master",
+			"committer": {
+				"name": "Theo Armour",
+				"email": "t.armour@gmail.com"
+			},
+			"content": codedData,
+			"message": "add to file",
+			"sha": logFileSha
+		}),
+
+	})
+		.then(response => response.json())
+		.then(data => {
+
+			divResponsePost.innerText += (JSON.stringify(data.content, null, "\t"));
+
+		})
+		.catch(err => console.warn('Something went wrong.', err));
+
+}
+
+function formatDoc(sCmd, sValue) {
+	if (validateMode()) {
+		document.execCommand(sCmd, false, sValue);
+		oDoc.focus();
+	}
+}
+
+function validateMode() {
+	if (!switchBox.checked) {
+		return true;
+	}
+	alert('Uncheck "Show HTML".');
+	oDoc.focus();
+	return false;
+}
+
+function setDocMode(bToSource) {
+	var oContent;
+	if (bToSource) {
+		oContent = document.createTextNode(oDoc.innerHTML);
+		oDoc.innerHTML = "";
+		var oPre = document.createElement("pre");
+		oDoc.contentEditable = false;
+		oPre.id = "sourceText";
+		oPre.contentEditable = true;
+		oPre.appendChild(oContent);
+		oDoc.appendChild(oPre);
+	} else {
+		if (document.all) {
+			oDoc.innerHTML = oDoc.innerText;
+		} else {
+			oContent = document.createRange();
+			oContent.selectNodeContents(oDoc.firstChild);
+			oDoc.innerHTML = oContent.toString();
+		}
+		oDoc.contentEditable = true;
+	}
+	oDoc.focus();
+}
+
+function printDoc() {
+	if (!validateMode()) {
+		return;
+	}
+	var oPrntWin = window.open(
+		"",
+		"_blank",
+		"width=450,height=470,left=400,top=100,menubar=yes,toolbar=no,location=no,scrollbars=yes"
+	);
+	oPrntWin.document.open();
+	oPrntWin.document.write(
+		'<!doctype html><html><head><title>Print<\/title><\/head><body onload="print();">' +
+		oDoc.innerHTML +
+		"<\/body><\/html>"
+	);
+	oPrntWin.document.close();
+}
+
+function openFile(files) {
+	var fileData, reader, data;
+
+	reader = new FileReader();
+	reader.onload = function (event) {
+		textBox.innerHTML = reader.result;
+
+		menuOpenFile.innerHTML =
+			"name: " +
+			files.files[0].name +
+			"<br>" +
+			"size: " +
+			files.files[0].size.toLocaleString() +
+			" bytes<br>" +
+			"type: " +
+			files.files[0].type +
+			"<br>" +
+			"modified: " +
+			files.files[0].lastModifiedDate.toLocaleDateString() +
+			"";
+
+		//	console.log( '', files );
+	};
+
+	reader.readAsText(files.files[0]);
+}
+
+function saveFile() {
+	const blob = new Blob([textBox.innerHTML]);
+	let a = document.body.appendChild(document.createElement("a"));
+	a.href = window.URL.createObjectURL(blob);
+	a.download = "hello-world.html";
+	a.click();
+	a = null;
+}
+
+initDoc();
